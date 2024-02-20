96cb47eeecd8aa38cd3182bc9a53d289535f7a37
ben <sobenkh35@gmail.com>
2024-02-20 18:31:24 +0700

    first commit

---
+++ components/CourseCard.js
@@ -0,0 +1,83 @@
+const courseCard=(course)=>{
+    return `
+    
+    <div class="flip-card w-full max-w-sm ">
+        <div class="flip-card-inner">
+            
+
+            <div class="flip-card-front">
+                <!-- Original content goes here -->
+                <div
+                class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div><img src="https://img-c.udemycdn.com/course/480x270/567828_67d0.jpg" alt=""></div>
+                <div><h1 class="text-center">The Complete Python Bootcamp From Zero to Hero in Python</h1></div>
+                <ul role="list" class="space-y-5 my-7">
+                    <li class="flex items-center">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+                    <li class="flex">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+                    <li class="flex">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+        
+                </ul>
+            </div>
+            </div>
+            <!-- Back of the card -->
+            <div
+                class="flip-card-back ">
+                <!-- Content for the back of the card -->
+
+                <div
+                class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div><img src="https://img-c.udemycdn.com/course/480x270/567828_67d0.jpg" alt=""></div>
+                <div><h1 class="text-center">The Complete Python Bootcamp From Zero to Hero in Python</h1></div>
+                <ul role="list" class="space-y-5 my-7">
+                    <li class="flex items-center">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+                    <li class="flex">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+                    <li class="flex">
+                        <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">You will learn how to leverage the power of Python to solve tasks.</span>
+                    </li>
+        
+                </ul>
+            </div>
+            </div>
+        </div>
+    </div>`
+}
